82711389f4b4a7640f46802fe2d06a0a2374da30
ChiakiUdon <chesna21@gmail.com>
2026-04-29 20:01:19 +0700

    Import and add path to ForgetPassword

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import Dashboard from '../views/Dashboard.vue'
+import ForgetPassword from '../views/ForgetPassword.vue';
 
 const routes = [
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/ForgetPassword', name: 'ForgetPassword', component: ForgetPassword },
   { path: '/', redirect: '/login' },
   { path: '/profile', name: 'profile',component: () => import('../views/Profile.vue')
 }
