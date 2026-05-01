947979d4d1d6bb576ac17d57df3b3afda8fe1244
ChiakiUdon <chesna21@gmail.com>
2026-05-01 12:59:15 +0700

    Import and add route to ActivityLogs.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,14 @@ import Register from '../views/Register.vue'
 import Dashboard from '../views/Dashboard.vue'
 import FolderView from '../views/FolderView.vue'
 import ForgetPassword from '../views/ForgetPassword.vue';
+import ActivityLog from '../views/ActivityLog.vue';
 
 const routes = [
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/ForgetPassword', name: 'ForgetPassword', component: ForgetPassword },
+  { path: '/activity', name: 'ActivityLog', component: ActivityLog },
   { path: '/', redirect: '/login' },
   { path: '/profile', name: 'profile',component: () => import('../views/Profile.vue') },
   { path: '/folder/:id', name: 'FolderView', component: FolderView, props: true }
